42a1cd1fca7f31835e06250cc8bf8376c87564fb
dustyw0lf <dustyw0lf@users.noreply.github.com>
2023-02-11 11:53:39 +0200

    Changed failure message in main

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
-        .expect("Failed to connect to Postgres.");
+        .expect("Failed to connect to database");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
